684e3032a38b9d425dae5ecbd8e669455843d211
Martin Salles <martinsalles@MacBook-Pro-de-Martin.local>
2017-01-10 16:28:15 +0100

    Day 11 video player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,54 @@
+const video = document.querySelector('video');
+const playButton = document.querySelector('.toggle');
+const volumeSlider = document.querySelector('input[name="volume"');
+const speedSlider = document.querySelector('input[name="playbackRate"]');
+const plus25Button = document.querySelector('button[data-skip="25"]');
+const minus10Button = document.querySelector('button[data-skip="-10"]');
+const progressBar = document.querySelector('.progress');
+const progressBarFilled = document.querySelector('.progress__filled');
+
+togglePlay = () => {
+  if (video.paused) {
+    video.play();
+    playButton.innerText = '❚❚';
+  } else {
+    video.pause();
+    playButton.innerText = '►';
+  }
+}
+
+updateVolume = () => {
+  video.volume = volumeSlider.value;
+}
+
+updateSpeed = () => {
+  video.playbackRate = speedSlider.value;
+}
+
+forward = () => {
+  video.currentTime += 25;
+}
+
+backward = () => {
+  video.currentTime -= 10;
+}
+
+updateProgressBar = () => {
+  progressBarFilled.style.flexBasis = `${video.currentTime / video.duration * 100}%`;
+}
+
+changeCurrentTime = (e) => {
+  const time = e.offsetX / progressBar.offsetWidth * video.duration;
+  video.currentTime = time;
+}
+
+playButton.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+volumeSlider.addEventListener('change', updateVolume);
+speedSlider.addEventListener('change', updateSpeed);
+plus25Button.addEventListener('click', forward);
+minus10Button.addEventListener('click', backward);
+
+video.addEventListener('timeupdate', updateProgressBar);
+
+progressBar.addEventListener('click', changeCurrentTime);
